7688fc301eb3747394548363d5d1e92dc2873cee
Quan Tran <tranminhquan97@gmail.com>
2017-04-12 21:37:32 -0400

    Add a color struct instead of using a single double

--- graphic.h
+++ graphic.h
@@ -5,11 +5,18 @@
 #ifndef PARTICLE_SIM_GRAPHIC_H
 #define PARTICLE_SIM_GRAPHIC_H
 
+#include <stdio.h>
 #include <GL/glut.h>
 #include "fluid.h"
 
+typedef struct {
+    double r;
+    double b;
+    double g;
+} color_t;
+
 void init_render();
-void draw_cube(FluidCube *cube, int n);
-void draw_square(int x, int y, double vsize, double hsize, double color);
+void draw_cube(FluidCube *cube);
+void draw_square(int x, int y, double vsize, double hsize, color_t* color);
 
 #endif //PARTICLE_SIM_GRAPHIC_H
