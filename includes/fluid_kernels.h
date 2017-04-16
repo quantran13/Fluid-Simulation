2c374fcbe1ca4550e0f9d550d74822516b34255b
Quan Tran <tranminhquan97@gmail.com>
2017-04-16 19:43:12 -0400

    Fixed declaration errors

--- includes/fluid_kernels.h
+++ includes/fluid_kernels.h
@@ -5,7 +5,7 @@
 #ifndef FLUID_SIMULATION_FLUID_KERNELS_H
 #define FLUID_SIMULATION_FLUID_KERNELS_H
 
-__device__ void advect_kernel(double *d, double *d0, double *vecX, double *vecY,
+__global__ void advect_kernel(double *d, double *d0, double *vecX, double *vecY,
                               double *vecZ, double dt, int N);
 
 #endif //FLUID_SIMULATION_FLUID_KERNELS_H
--- src/fluid_kernels.cu
+++ src/fluid_kernels.cu
@@ -2,7 +2,7 @@
 // Created by quan on 4/16/17.
 //
 
-#include "fluid_kernels.h"
+#include <fluid_kernels.h>
 
 __global__ void advect_kernel(double *d, double *d0, double *vecX, double *vecY,
                               double *vecZ, double dt, int N)
