0737acfe0be6adcea004f835e7cda811d131e208
Quan Tran <tranminhquan97@gmail.com>
2017-04-17 19:40:52 -0400

    Fixed declaration of set_bnd_kernel in header file

--- includes/fluid_kernels.h
+++ includes/fluid_kernels.h
@@ -11,5 +11,9 @@
 
 __global__ void advect_kernel(double *d, double *d0, double *vecX, double *velocY,
                               double *lo, double dt, int N, int k);
+__global__ void set_bnd_kernel(int b, double *x, int N);
+__global__ void project_kernel(double *velocX, double *velocY, double *velocZ,
+                               double *p, double *div, int iter, int N,
+                               double N_recip, int k);
 
 #endif //FLUID_SIMULATION_FLUID_KERNELS_H
